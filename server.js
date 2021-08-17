75e58e1565a2bcb27df48514dc728ba109ef80d5
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-08-17 16:11:33 +0300

    server work

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 
 
 
-const express = require ('express')
+const express = require ('express')  ////  as import library 
+const cors = require('cors')         ////
+
+
+require('dotenv').config();
 
 
 const server = express()
 
 const port  = 7000 
+
+const  wheatherData = require ('./Data.json')
 
 
 server.listen(port,()=>{              /////// preaper server listen request//////
@@ -22,5 +28,48 @@ server.listen(port,()=>{              /////// preaper server listen request/////
 server.get('/test',(req,res)=>{
         res.send(' its working ')
 })
+
+
+class weather{
+    constructor(item){
+        this.date=item.valid_date;
+        this.description = item.weather.description; //// in jason  
+    }
+}
+
+
+
+
+//localHost:7000/wheather?cityName=Amman  
+
+
+server.get('/wheather',(req,res) =>{
+            let searchQury = req.query.cityName /// to make request diynamk ... to acces after ? 
+
+        let cityData = wheatherData.find( city =>{
+            if ( city.city_name === searchQury ) { /////// 
+               
+                return city;
+            }
+        })
+
+        console.log(cityData);
+
+
+        let weatherArr=cityData.data.map((item)=>{
+            console.log(item)
+        
+            return new weather(item);
+        })
+
+        
+   res.send(weatherArr)
+})
+
+
+
+
+
+
 
 
