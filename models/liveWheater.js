15adb829e0f59282c72734a926c5d003339d4129
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-08-21 23:02:13 +0300

    finsh lab 10

--- models/liveWheater.js
+++ models/liveWheater.js
@@ -1,39 +1,55 @@
 let axios=require('axios');
 module.exports=handelWheather;
 
-
+let inMemory={};
 
 
 async function handelWheather(req,res) {
     
     let searchLiveWheater = req.query.city
+   
+    let wheaterLiveUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WHEATHER_KEY_live}&city=${searchLiveWheater}`;
     
-
-    try{
-            let  wheaterLiveUrl = `${process.env.WHEATHER_KEY_live}/daily?&key=${process.env.WHEATHER_KEY}&city=${searchLiveWheater}&days=5`	
-
-               let wheaterLiveResult = await axios.get(wheaterLiveUrl) 
-       
-        console.log('kkkkkk',wheaterLiveUrl);
-       
-        // res.send(wheaterLiveResult.data)  
-
-        let wheatherData = wheaterLiveResult.data.data.map(info=>{
-
-            return new forecastWheater(info)
-        })
-
-              res.send(wheatherData);
+    if(inMemory[searchLiveWheater] != undefined){
+        console.log(' cache hit , data in cache memory');
+        res.send(inMemory[searchLiveWheater]);
 
     }
 
+         else{
 
-catch (error) {
-    console.log('eeeeee',error);
-    // res.send('eroor from axios') 
+        try{
+    
+   
+            let newArrr=[];
+        
+            let wheatherDat= await axios.get(wheaterLiveUrl);
+              
+            let weatherArr=wheatherDat.data.data
+                for(let i=0;i<5;i++){
+                let newOb=new forecastWheater(weatherArr[i]);
+                
+                newArrr.push(newOb);
+                }
+
+                inMemory[searchLiveWheater]=newArrr;
+                console.log('cache miss');
+                res.send(newArrr);
+                }
+           
+            catch (error) {
+                        res.send(error)
+            }
+
+    }
+    
+ 
+    
 }
 
-}
+  
+
+
 
 
 class forecastWheater {
